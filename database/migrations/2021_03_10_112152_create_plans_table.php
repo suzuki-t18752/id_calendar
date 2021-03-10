6105961db67a55f17e13163a88a33f68347d11a0
t.suzuki <tkft.psy@gmail.com>
2021-03-10 20:55:07 +0900

    plan_migrate_file_create

---
+++ database/migrations/2021_03_10_112152_create_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id', 30);
+            $table->date('start_date');
+            $table->date('end_date')->nullable()->change();
+            $table->string('title', 100);
+            
+            $table->integer('open_flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
